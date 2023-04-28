1420fc755197e80b31d598d2cb37cef57888888c
prabhakarikonicit <prabhakar.koppireddy@ikonicit.com>
2023-04-28 17:00:39 +0530

    war changes
    war changes

--- src/main/java/com/ectd/global/eln/ElnApplication.java
+++ src/main/java/com/ectd/global/eln/ElnApplication.java
@@ -3,11 +3,12 @@ package com.ectd.global.eln;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan({"com.ectd.global.*"})
-public class ElnApplication {
+public class ElnApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElnApplication.class, args);
